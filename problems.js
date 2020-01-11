43bf70dd873086be4db754096b2314b68ee41c22
Logan Kilby <logankilby@gmail.com>
2020-01-11 16:09:28 -0600

    Update problems.js

--- problems.js
+++ problems.js
@@ -66,3 +66,9 @@ function gPrimeFactor() {
 	console.log(n);
 	console.log(array);
 }
+
+
+//Project euler #4:
+//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+//Find the largest palindrome made from the product of two 3-digit numbers.
+
